6874ab7f7b2e1419aa5194735e092a4894c8dc8e
EdgarCastaneda24110242 <a24110242@live.ceti.mx>
2025-03-25 23:05:56 +0000

    Version 0

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Foco.hpp"
+int main(int argc, char const *argv[])
+{
+    Foco focos[10];
+    for (size_t i = 0; i < 10; i++)
+    {
+        focos[1].Encender();
+    }
+
+    foco1.Encender();
+
+    std::cout
+        << "Foco 1:"
+        << foco1.LeerEstado()
+        << std::endl;
+
+    return 0;
+}
