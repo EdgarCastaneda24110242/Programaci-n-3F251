b69b907b2f2a38d20627971557aab9fc872a8ca0
EdgarCastaneda24110242 <a24110242@live.ceti.mx>
2025-04-30 00:16:03 +0000

    Practica 5

--- src/archivos.cpp
+++ src/archivos.cpp
@@ -2,6 +2,11 @@
 #include <fstream>
 #include <string>
 using namespace std;
+struct persona
+{
+    char nombre[12];
+    int edad;
+};
 int main(int argc, char const *argv[])
 {
     ofstream archivoSalida("archivo.txt");
@@ -11,7 +16,7 @@ int main(int argc, char const *argv[])
         return 1;
     }
     cout << "Escribiendo en el archivo" << endl;
-    archivoSalida << "12 + 1" << endl;
+    archivoSalida << "12 + 1" << endl; 
 
     archivoSalida.close();
     cout << "Archivo cerrado" << endl;
@@ -31,5 +36,29 @@ int main(int argc, char const *argv[])
     }
     archivoEntrada.close();
     cout << "Archivo cerrado" << endl;
+
+    //escritura de archivos binarios
+    ofstream archivoSalidaBinario("archivo.bin", ios::binary);
+    if (!archivoSalidaBinario)
+    {
+        cerr << "Error al abrir el archivo binario" << endl;
+        return 1;
+    }
+    persona p1 = {"Juan", 20};
+    persona p2 = {"Maria", 30};
+    persona p3 = {"Pedro", 40};
+
+    archivoSalidaBinario.write((char*)(&p1), sizeof(persona));
+    archivoSalidaBinario.write((char*)(&p2), sizeof(persona));  
+    archivoSalidaBinario.write((char*)(&p3), sizeof(persona));
+    archivoSalidaBinario.close();
+    cout << "Archivo binario cerrado" << endl;
+    // Lectura de archivos binarios
+
+
+
+
+
+
     return 0;
 }
